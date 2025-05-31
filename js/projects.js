5bd0d6de4f713b7aee99d60635398cc236400b8f
Moayad7 <moauayad.ahmad7@gmail.com>
2025-05-31 12:49:20 +0300

    1000

--- js/projects.js
+++ js/projects.js
@@ -1,84 +1,192 @@
 
+  // const projects = [
+  //   {
+  //     title: "Sheikh Hamid Road Project with Al Ayuni company",
+  //     station: "ST-0+00 to 10+000",
+  //     year: "September 2022",
+  //     image: "assets/projects/1/1.jpg",
+  //     link: "project-details-1.html"
+  //   },
+  //   {
+  //     title: "Spine Earth Work with Al-Shalawi Company",
+  //     station: "ST 56+200 to ST 58+200",
+  //     year: "August 2023 - April 2024",
+  //     image: "assets/projects/2/22.jpg",
+  //     link: "project-details-2.html"
+  //   },
+  //   {
+  //     title: "Spine project with Al-Suwailem company",
+  //     station: "",
+  //     year: "January 2023",
+  //     image: "assets/projects/3/3.jpg",
+  //     link: "project-details-3.html"
+  //   },
+  //   {
+  //     title: "Land levelling project, Qayal area With Mobco company",
+  //     station: "",
+  //     year: "January 2023 - March 2023",
+  //     image: "assets/projects/4/4.jpg",
+  //     link: "project-details-4.html"
+  //   },
+  //   {
+  //     title: "Expansion project of Al-Muwaileh airport road with Mobco company",
+  //     station: "",
+  //     year: "August 2022 - March 2023",
+  //     image: "assets/projects/5/5.jpg",
+  //     link: "project-details-5.html"
+  //   },
+  //   {
+  //     title: "Spine Earth Work with Al Ayuni company",
+  //     station: "St 56+200 to ST 58+200 ",
+  //     year: "August 2022 -April 2024",
+  //     image: "assets/logo-p-01.png",
+  //     // link: "project-details-5.html"
+  //   },
+  //   {
+  //     title: " Al-Rain Road project , at 33 km with AlShalawi Company",
+  //     station: "",
+  //     year: "2020",
+  //     image: "assets/logo-p-01.png",
+  //     // link: "project-details-5.html"
+  //   },
+  //   {
+  //     title: "Spine Earth Work with Al Shalawi company",
+  //     station: "ST 56+200 to ST 58+200",
+  //     year: "August 2023 -April 2024",
+  //     image: "assets/logo-p-01.png",
+  //     // link: "project-details-5.html"
+  //   },
+  //   {
+  //     title: "TSSM Early Works and Service Projects with CHINA FIRST HIGHWAY ENGINEERING CO company",
+  //     station: "3A Ski Slope & Early Works",
+  //     year: "August 2024-Till Now",
+  //     image: "assets/logo-p-01.png",
+  //     // link: "project-details-5.html"
+  //   },
+  //   {
+  //     title: "WTS-water Transmission System -Oxagon-NEOM with Petrojet company",
+  //     station: "",
+  //     year: "November 2024-Till Now",
+  //     image: "assets/logo-p-01.png",
+  //     // link: "project-details-5.html"
+  //   },
+  //   {
+  //     title: "WTS-water Transmission System -Oxagon-NEOM The wastewater Network project in the Badr District of Riyadh by National Water Company with Al Ayuni company",
+  //     station: "",
+  //     year: "April 2023",
+  //     image: "assets/logo-p-01.png",
+  //     // link: "project-details-5.html"
+  //   }
+    
+  // ];
+
   const projects = [
     {
-      title: "Sheikh Hamid Road Project with Al Ayuni company",
-      station: "ST-0+00 to 10+000",
-      year: "September 2022",
-      image: "assets/projects/1/1.jpg",
-      link: "project-details-1.html"
+        "title": "Al-Rain Road Project with Al-Shalawi Company",
+        "station": "St 33+000",
+        "client": "Al-Shalawi Company",
+        "location": "Rain Bisha",
+        "projectDate": "Aug-2020",
+        "image": "assets/projects/1/1.jpg",
+        "link": "project-details-1.html"
     },
     {
-      title: "Spine Earth Work with Al-Shalawi Company",
-      station: "ST 56+200 to ST 58+200",
-      year: "August 2023 - April 2024",
-      image: "assets/projects/2/22.jpg",
-      link: "project-details-2.html"
+        "title": "Earth Works at Work Order # 55 Expansion Project of Al-Muwaileh Airport Road",
+        "client": "Mobco company",
+        "location": "Neom",
+        "projectDate": "Jan-2022",
+        "image": "assets/projects/2/1.jpg",
+        "link": "project-details-2.html"
     },
     {
-      title: "Spine project with Al-Suwailem company",
-      station: "",
-      year: "January 2023",
-      image: "assets/projects/3/3.jpg",
-      link: "project-details-3.html"
+        "title": "Sheikh Hamid Road Project",
+        "station": "St 00+000 to St 10+000",
+        "client": "Alayuni Company",
+        "location": "Neom",
+        "projectDate": "Sept-2022",
+        "image": "assets/projects/3/1.jpg",
+        "link": "project-details-3.html"
     },
     {
-      title: "Land levelling project, Qayal area With Mobco company",
-      station: "",
-      year: "January 2023 - March 2023",
-      image: "assets/projects/4/4.jpg",
-      link: "project-details-4.html"
+        "title": "Earth Works at Work Order #35 Spine Project",
+        "station": "St 57+200 to St 58+200",
+        "client": "Alayuni Company",
+        "location": "Neom",
+        "projectDate": "Aug-2022",
+        "image": "assets/projects/4/1.jpg",
+        "link": "project-details-4.html"
     },
     {
-      title: "Expansion project of Al-Muwaileh airport road with Mobco company",
-      station: "",
-      year: "August 2022 - March 2023",
-      image: "assets/projects/5/5.jpg",
-      link: "project-details-5.html"
+        "title": "Earth Works at Work Order #35 Spine Project",
+        "station": "St 57+200 to St 58+200",
+        "client": "Alayuni Company",
+        "location": "Neom",
+        "projectDate": "Aug-2022",
+        "image": "assets/projects/5/1.jpg",
+        "link": "project-details-5.html"
     },
     {
-      title: "Spine Earth Work with Al Ayuni company",
-      station: "St 56+200 to ST 58+200 ",
-      year: "August 2022 -April 2024",
-      image: "assets/logo-p-01.png",
-      // link: "project-details-5.html"
+        "title": "Land Levelling Project at Qayal Area",
+        "client": "Mobco company",
+        "location": "Neom",
+        "projectDate": "Jan-2023",
+        "image": "assets/projects/6/1.jpg",
+        "link": "project-details-6.html"
     },
     {
-      title: " Al-Rain Road project , at 33 km with AlShalawi Company",
-      station: "",
-      year: "2020",
-      image: "assets/logo-p-01.png",
-      // link: "project-details-5.html"
+        "title": "Earth Works at Work Order # 9 Spine Project",
+        "station": "St 71+000",
+        "client": "Al-Suwailem company",
+        "location": "Neom",
+        "projectDate": "Jan-2023",
+        "image": "assets/projects/7/1.jpg",
+        "link": "project-details-7.html"
     },
     {
-      title: "Spine Earth Work with Al Shalawi company",
-      station: "ST 56+200 to ST 58+200",
-      year: "August 2023 -April 2024",
-      image: "assets/logo-p-01.png",
-      // link: "project-details-5.html"
+        "title": "Earth Works at Work Order # 19 Spine Project",
+        "station": "St 56+200 to ST 58+200",
+        "client": "Al-Shalawi Company",
+        "location": "Neom",
+        "projectDate": "Aug-2023",
+        "image": "assets/projects/8/1.jpg",
+        "link": "project-details-8.html"
     },
     {
-      title: "TSSM Early Works and Service Projects with CHINA FIRST HIGHWAY ENGINEERING CO company",
-      station: "3A Ski Slope & Early Works",
-      year: "August 2024-Till Now",
-      image: "assets/logo-p-01.png",
-      // link: "project-details-5.html"
+        "title": "Infrastructure Works at The Wastewater Network project in Badr District of Riyadh",
+        "client": "SALAT Contracting Company",
+        "location": "Riyadh",
+        "projectDate": "May-2024",
+        "image": "assets/projects/9/1.jpg",
+        "link": "project-details-9.html"
     },
     {
-      title: "WTS-water Transmission System -Oxagon-NEOM with Petrojet company",
-      station: "",
-      year: "November 2024-Till Now",
-      image: "assets/logo-p-01.png",
-      // link: "project-details-5.html"
+        "title": "Earth Works at Spine project",
+        "client": "Mobco company",
+        "location": "Neom",
+        "projectDate": "Jan-2022",
+        "image": "assets/projects/10/1.jpg",
+        "link": "project-details-10.html"
     },
     {
-      title: "WTS-water Transmission System -Oxagon-NEOM The wastewater Network project in the Badr District of Riyadh by National Water Company with Al Ayuni company",
-      station: "",
-      year: "April 2023",
-      image: "assets/logo-p-01.png",
-      // link: "project-details-5.html"
+        "title": "Earthwork and Drainage for Ski Slope Snowmaking in Trojena",
+        "client": "China First Highway Engineering Co. Ltd Branch",
+        "location": "Neom",
+        "projectDate": "Aug 2024",
+        "image": "assets/projects/11/1.jpg",
+        "link": "project-details-11.html"
+    },
+    {
+        "title": "Infrastructure Works of the Pump Station Project - Oxagon-Neom",
+        "client": "Petrojet Company",
+        "location": "Neom",
+        "projectDate": "Nov 2024",
+        "image": "assets/projects/12/1.jpg",
+        "link": "project-details-12.html"
     }
-    
-  ];
+];
+
+
+
 
   const projectContainer = document.getElementById('project-container');
 
@@ -92,7 +200,7 @@
       <div>
         <h3 class="text-xl font-bold mb-4">${project.title}</h3>
         ${project.station ? `<p class="mb-1"><strong>Station:</strong> ${project.station}</p>` : ''}
-        <p class="mb-2"><strong>Project Year:</strong> ${project.year}</p>
+        <p class="mb-2"><strong>Project Year:</strong> ${project.projectDate}</p>
         </div>
         <div>
         ${project.link ? `<a href="${project.link}" class="inline-block px-4 py-2 bg-[#E2CE86] text-gray-800 rounded-xl hover:bg-[#C2A68A] transition duration-300">Read More</a>` : ''}
